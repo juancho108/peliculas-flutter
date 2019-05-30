c33e1f4bf2712cb136d3cec8dbc69e8ffdd8a291
juancho108 <jjf108@gmail.com>
2019-05-30 20:11:21 -0300

    subiendo primeras cosas de la api

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:peliculas/src/providers/peliculas_provider.dart';
 
 import 'package:peliculas/src/widgets/card_swiper_widget.dart';
 
@@ -27,6 +28,9 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _swiperTarjetas() {
+    final peliculasProvider = new PeliculasProvider();
+    peliculasProvider.getEnCines();
+
     return CardSwiper(
       peliculas: [1, 2, 3, 4, 5],
     );
---
+++ lib/src/providers/peliculas_provider.dart
@@ -0,0 +1,25 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import 'package:peliculas/src/models/pelicula_model.dart';
+
+class PeliculasProvider {
+  String _apikey = 'f02f27dcc375a0f273bfc120a646e037';
+  String _url = 'api.themoviedb.org';
+  String _language = 'es-ES';
+
+  Future<List<Pelicula>> getEnCines() async {
+    final url = Uri.https(
+      _url,
+      '3/movie/now_playing',
+      {'api_key': _apikey, 'language': _language},
+    );
+
+    final resp = await http.get(url);
+    final decodedData = json.decode(resp.body);
+
+    print(decodedData['results']);
+
+    return [];
+  }
+}
