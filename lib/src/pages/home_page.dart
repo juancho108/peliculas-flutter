25e00b0a8ab5f73dfb6fd6ecf9c70ee0288d8765
juancho108 <jjf108@gmail.com>
2019-06-06 16:54:20 -0300

    app optimizada y funcionando tarjetas de arriba y abajo de forma perfecta

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -9,6 +9,8 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    peliculasProvider.getPopulares();
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Peliculas'),
@@ -61,11 +63,14 @@ class HomePage extends StatelessWidget {
                 child: Text('Populares',
                     style: Theme.of(context).textTheme.subhead)),
             SizedBox(height: 5.0),
-            FutureBuilder(
-              future: peliculasProvider.getPopulares(),
+            StreamBuilder(
+              stream: peliculasProvider.popularesStream,
               builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
                 if (snapshot.hasData) {
-                  return MovieHorizontal(peliculas: snapshot.data);
+                  return MovieHorizontal(
+                    peliculas: snapshot.data,
+                    siguientePagina: peliculasProvider.getPopulares,
+                  );
                 } else {
                   return Center(child: CircularProgressIndicator());
                 }
--- lib/src/providers/peliculas_provider.dart
+++ lib/src/providers/peliculas_provider.dart
@@ -1,5 +1,7 @@
 import 'package:http/http.dart' as http;
+
 import 'dart:convert';
+import 'dart:async';
 
 import 'package:peliculas/src/models/pelicula_model.dart';
 
@@ -7,6 +9,25 @@ class PeliculasProvider {
   String _apikey = 'f02f27dcc375a0f273bfc120a646e037';
   String _url = 'api.themoviedb.org';
   String _language = 'es-ES';
+
+  int _popularesPage = 0;
+  bool _cargando = false;
+
+  List<Pelicula> _populares = new List();
+
+  final _popularesStreamController =
+      StreamController<List<Pelicula>>.broadcast();
+
+  //La primera parte es lo que le mando al stream. Le digo especificamente que va a recibir. un LIST DE PELICULAS
+  Function(List<Pelicula>) get popularesSink =>
+      _popularesStreamController.sink.add;
+
+  Stream<List<Pelicula>> get popularesStream =>
+      _popularesStreamController.stream;
+
+  void disposeStreams() {
+    _popularesStreamController?.close();
+  }
 
   Future<List<Pelicula>> _procesarRespuesta(Uri url) async {
     final resp = await http.get(url);
@@ -28,12 +49,30 @@ class PeliculasProvider {
   }
 
   Future<List<Pelicula>> getPopulares() async {
+    if (_cargando) return [];
+
+    _cargando = true;
+
+    _popularesPage++;
+
     final url = Uri.https(
       _url,
       '3/movie/popular',
-      {'api_key': _apikey, 'language': _language},
+      {
+        'api_key': _apikey,
+        'language': _language,
+        'page': _popularesPage.toString(),
+      },
     );
 
-    return await _procesarRespuesta(url);
+    final resp = await _procesarRespuesta(url);
+
+    _populares.addAll(resp);
+
+    popularesSink(_populares);
+
+    _cargando = false;
+
+    return resp;
   }
 }
